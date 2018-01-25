cf6570a8fb31e11fef3dafe106880910815e94c3
Unknown <binghamstudent@live.com>
2018-01-25 16:52:38 -0700

    Added other animals with other names and functions

--- Assets/Animal.cs
+++ Assets/Animal.cs
@@ -6,10 +6,16 @@ public class Animal : MonoBehaviour {
 
 public string Name;
 public int FoodCount;
+public string Dogsname;
+public float Math;
+public string Name3;
 void OnMouseDown()
 {
 	print("I have a " + Name);
 	print("It has this amount of food: " + FoodCount);
+	print("The "+ Dogsname + " ate half of the " + Name +"'s food");
+	print("There is now " + Math + " left in the dog bowl");
+	print("Now " + Name3 + " is going to starve");
 
 }
 
