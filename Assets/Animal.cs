05e24e703fb9b6672a539c57d7530632997948aa
Unknown <binghamstudent@live.com>
2018-01-25 11:24:49 -0700

    used variable to name animal

--- Assets/Animal.cs
+++ Assets/Animal.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		print("My animal is eating.");
-		print("Now my animal is sleeping.");
-		print("I love my cat");
+public string Name;
+public int FoodCount;
+void OnMouseDown()
+{
+	print("I have a " + Name);
+	print("It has this amount of food: " + FoodCount);
 
-	}
+}
+
 	
-	void OnMouseDown()
-	{
-		print("Meow");
-	}
-	// Update is called once per frame
-	void Update () {
 		
 	}
-}
+
