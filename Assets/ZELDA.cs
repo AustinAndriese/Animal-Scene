b8054b4c48166eacfcae33fdf5d07aa0b453f4ea
Unknown <binghamstudent@live.com>
2018-02-06 12:06:10 -0700

    finalizing this repository and moving to a new one

---
+++ Assets/ZELDA.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZELDA : MonoBehaviour {
+    public bool Key;
+    public int Health = 3;
+    public bool BossKey;
+    public string Name;
+    public int HeartMeter;
+    public string Password = "Hyrule";
+    void Start() {
+        if(HeartMeter > 0 && HeartMeter == 3)
+        {
+            print("You are full health.");
+        }
+        if(Health < 3 && Health > 0)
+        {
+            print("You are low on health, restore soon.");
+        }
+        if(BossKey && Health > 2)
+        {
+            print("You are ready for batttle.");
+        }
+        if(Password == "Hyrule" || Password == "hyrule" || Password == "HYRULE");
+        {
+            print("You may enter!");
+        }
+
+    }
+}
+
